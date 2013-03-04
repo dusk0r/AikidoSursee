8227635a2a9de6b6054734158575dedd57df135c
chris <chris@41c99623-bf33-be40-8e60-8435aa2e7e80>
2013-03-04 22:03:22 +0000

    Glossar und FAQ
    git-svn-id: https://aikido.amigo-online.ch/svn/AikidoSursee/trunk@35 41c99623-bf33-be40-8e60-8435aa2e7e80

---
+++ Main/AikidoWebsite.Data/Entities/FaqEintrag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AikidoWebsite.Data.Entities {
+    
+    public class FaqEintrag : IEntity {
+        public string Id { get; set; }
+        public string Frage { get; set; }
+        public string Text { get; set; }
+
+    }
+}
---
+++ Main/AikidoWebsite/Controllers/AikidoInfosController.cs
@@ -0,0 +1,70 @@
+﻿using AikidoWebsite.Common;
+using AikidoWebsite.Service.Validator;
+using AikidoWebsite.Web.Models;
+using Raven.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AikidoWebsite.Web.Extensions;
+using AikidoWebsite.Data.ValueObjects;
+using AikidoWebsite.Data.Entities;
+using AikidoWebsite.Data;
+
+namespace AikidoWebsite.Web.Controllers {
+    public class AikidoInfosController : Controller {
+
+        [Inject]
+        public IDocumentSession DocumentSession { get; set; }
+
+        [Inject]
+        public IValidatorService ValidatorService { get; set; }
+
+        [HttpGet]
+        public ActionResult Faq() {
+            var model = new FaqModel {
+                IsAdmin = User.IsInGroup(Gruppe.Admin),
+                Entries = DocumentSession.Query<FaqEintrag>()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [RequireGruppe(Gruppe.Admin)]
+        public JsonResult Faq(IEnumerable<FaqEintrag> entries) {
+            foreach (var entry in entries) {
+                ValidatorService.Validate(entry);
+                DocumentSession.Store(entry);
+            }
+            DocumentSession.SaveChanges();
+
+            return Json(true);
+        }
+
+        public ActionResult Glossar() {
+            var model = new GlossarModel {
+                IsAdmin = User.IsInGroup(Gruppe.Admin),
+                Entries = DocumentSession.Query<GlossarEintrag>()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [RequireGruppe(Gruppe.Admin)]
+        public JsonResult Glossar(IEnumerable<GlossarEintrag> entries) {
+            foreach (var entry in entries) {
+                ValidatorService.Validate(entry);
+                DocumentSession.Store(entry);
+            }
+            DocumentSession.SaveChanges();
+
+            return Json(true);
+        }
+
+        public ActionResult Techniken() {
+            return View();
+        }
+
+    }
+}
