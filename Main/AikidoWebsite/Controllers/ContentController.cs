0ffb4d28b924968475c4a3e2939cf464db3e695a
chris <chris@41c99623-bf33-be40-8e60-8435aa2e7e80>
2013-09-22 19:42:27 +0000

    Vorbereitung für Content
    git-svn-id: https://aikido.amigo-online.ch/svn/AikidoSursee/trunk@52 41c99623-bf33-be40-8e60-8435aa2e7e80

---
+++ Main/AikidoWebsite/Controllers/ContentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AikidoWebsite.Web.Controllers {
+
+    public class ContentController : Controller {
+
+        public ActionResult View(string id) {
+            return View();
+        }
+
+        public ActionResult Edit(string id) {
+            return View();
+        }
+
+    }
+}
