1543778187d808ac714db2da1ccc87b63bc5ffd5
chris <chris@41c99623-bf33-be40-8e60-8435aa2e7e80>
2014-03-27 22:39:51 +0000

    Fehlerseiten umgeleitet
    git-svn-id: https://aikido.amigo-online.ch/svn/AikidoSursee/trunk@117 41c99623-bf33-be40-8e60-8435aa2e7e80

---
+++ Main/AikidoWebsite/Controllers/ErrorController.cs
@@ -0,0 +1,34 @@
+﻿using AikidoWebsite.Common;
+using Castle.Core.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AikidoWebsite.Web.Controllers {
+    public class ErrorController : Controller {
+
+        [Inject]
+        public ILogger Logger { get; set; }
+
+        public ActionResult NotFound() {
+            Response.StatusCode = (int)HttpStatusCode.NotFound;
+            return View();
+        }
+
+        public ActionResult Unauthorized() {
+            Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            return View();
+        }
+
+        public ActionResult ServerError() {
+            Logger.Fatal("Serverfehler aufgetreten");
+
+            Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            return View();
+        }
+
+    }
+}
