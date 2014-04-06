1b6ed7f741546c459f5bf465f24d6514f5084331
chris <chris@41c99623-bf33-be40-8e60-8435aa2e7e80>
2014-04-06 21:49:07 +0000

    Extension hinzugefügt
    git-svn-id: https://aikido.amigo-online.ch/svn/AikidoSursee/trunk@120 41c99623-bf33-be40-8e60-8435aa2e7e80

---
+++ Main/AikidoWebsite/Extensions/JsonExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AikidoWebsite.Web.Extensions {
+    
+    public static class JsonExtensions {
+        private static readonly JsonSerializerSettings Settings = new JsonSerializerSettings {
+            DateFormatHandling = DateFormatHandling.IsoDateFormat,
+            DateTimeZoneHandling = DateTimeZoneHandling.Local,
+        };
+
+        public static string Serialize(object obj) {
+            return JsonConvert.SerializeObject(obj, Settings);
+        }
+    }
+}
