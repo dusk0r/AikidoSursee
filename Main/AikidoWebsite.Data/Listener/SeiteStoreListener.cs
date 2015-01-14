3cce279c714b42eb3c159e06a77c3ac69b124634
dusk0r <chris@amigo-online.ch>
2015-01-14 21:07:01 +0100

    Verwendete Dateien von Mitteilungen und Seiten in Metadaten speichern

--- Main/AikidoWebsite.Data/Listener/SeiteStoreListener.cs
+++ Main/AikidoWebsite.Data/Listener/SeiteStoreListener.cs
@@ -16,17 +16,19 @@ namespace AikidoWebsite.Data.Listener {
 
         public bool BeforeStore(string key, object entityInstance, Raven.Json.Linq.RavenJObject metadata, Raven.Json.Linq.RavenJObject original) {
             var seite = entityInstance as Seite;
+            var mitteilung = entityInstance as Mitteilung;
+
+            if (metadata.ContainsKey("MarkupFiles")) {
+                metadata.Remove("MarkupLinks");
+            }
 
             if (seite != null) {
-                var linkMatches = CreoleLink.Matches(seite.WikiCreole ?? "");
-                var imageMatches = CreoleImage.Matches(seite.WikiCreole ?? "");
+                metadata.Add("MarkupFiles", new RavenJArray(ExtractFiles(seite.WikiCreole)));
+                return true;
+            }
 
-                var links = linkMatches.Cast<Match>().Where(m => m.Success).Select(m => m.Groups[2]).Select(g => g.Value);
-                var images = imageMatches.Cast<Match>().Where(m => m.Success).Select(m => m.Groups[2]).Select(g => g.Value);
-
-                metadata.Add("MarkupLinks", new RavenJArray(links));
-                metadata.Add("MarkupImages", new RavenJArray(images));
-
+            if (mitteilung != null) {
+                metadata.Add("MarkupFiles", new RavenJArray(ExtractFiles(mitteilung.Text)));
                 return true;
             }
 
@@ -35,6 +37,25 @@ namespace AikidoWebsite.Data.Listener {
 
         public void AfterStore(string key, object entityInstance, Raven.Json.Linq.RavenJObject metadata) {
             
+        }
+
+        private static IEnumerable<string> ExtractFiles(string creole) {
+            var linkMatches = CreoleLink.Matches(creole ?? "");
+            var imageMatches = CreoleImage.Matches(creole ?? "");
+
+            var links = linkMatches.Cast<Match>().Where(m => m.Success).Select(m => m.Groups[2]).Select(g => g.Value).Select(StripLinkId);
+            var images = imageMatches.Cast<Match>().Where(m => m.Success).Select(m => m.Groups[2]).Select(g => g.Value);
+
+            return links.Concat(images);
+        }
+
+        private static string StripLinkId(string link) {
+            if (link.Contains('|')) {
+                var parts = link.Split('|');
+                return parts[0];
+            } else {
+                return link;
+            }
         }
 
 
