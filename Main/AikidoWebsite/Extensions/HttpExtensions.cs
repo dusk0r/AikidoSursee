b9278cfdbc3c3982788f3ad38438c7b5e6bd4b26
Christoph Enzmann <christoph.enzmann@confer.ch>
2018-06-22 15:57:26 +0200

    Unterstütztung für Proxies

--- Main/AikidoWebsite/Extensions/HttpExtensions.cs
+++ Main/AikidoWebsite/Extensions/HttpExtensions.cs
@@ -8,11 +8,34 @@ namespace AikidoWebsite.Web.Extensions
         public static string GetBaseUrl(this HttpContext context)
         {
             var builder = new StringBuilder();
-            builder.Append(context.Request.IsHttps ? "https://" : "http://");
-            builder.Append(context.Request.Host);
-            builder.Append("/");
+
+            if (context.Request.Headers.ContainsKey("X-Forwarded-Host"))
+            {
+                builder.Append(GetForwardedProto(context));
+                builder.Append("://");
+                builder.Append(context.Request.Headers["X-Forwarded-Host"]);
+                builder.Append("/");
+            }
+            else
+            {
+                builder.Append(context.Request.IsHttps ? "https://" : "http://");
+                builder.Append(context.Request.Host);
+                builder.Append("/");
+            }
 
             return builder.ToString();
+        }
+
+        private static string GetForwardedProto(HttpContext context)
+        {
+            switch (context.Request.Headers["X-Forwarded-Proto"])
+            {
+                case "http":
+                    return "http";
+                case "https":
+                default:
+                    return "https";
+            }
         }
     }
 }
