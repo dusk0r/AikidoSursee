4dd9ba6a72ff6ed88f1f9959bf6c3459680211e9
Christoph Enzmann <christoph.enzmann@confer.ch>
2018-08-16 13:41:27 +0200

    Caching für FlickrService

--- Main/AikidoWebsite/Service/FlickrService.cs
+++ Main/AikidoWebsite/Service/FlickrService.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using FlickrNet;
 
 namespace AikidoWebsite.Web.Service
@@ -6,21 +8,34 @@ namespace AikidoWebsite.Web.Service
 
     public class FlickrService {
         private readonly Flickr Flickr;
+        private readonly IDictionary<string, (DateTime, object)> Cache = new Dictionary<string, (DateTime, object)>();
+        private static readonly TimeSpan CacheTime = TimeSpan.FromHours(1);
 
         public FlickrService(string apiKey) {
             this.Flickr = new Flickr(apiKey);
         }
 
         public Task<PhotosetCollection> ListPhotosetsAsync() {
-            var t = new TaskCompletionSource<PhotosetCollection>();
-            Flickr.PhotosetsGetListAsync("128101479@N04", r => t.TrySetResult(r.Result));
-            return t.Task;
+            return TryGetFromCacheAsync<PhotosetCollection>("photosets", t => Flickr.PhotosetsGetListAsync("128101479@N04", r => t.TrySetResult(r.Result)));
         }
 
         public Task<PhotosetPhotoCollection> ListPhotosAsync(string galleryId) {
-            var t = new TaskCompletionSource<PhotosetPhotoCollection>();
-            Flickr.PhotosetsGetPhotosAsync(galleryId, r => t.TrySetResult(r.Result));
-            return t.Task;
+            return TryGetFromCacheAsync<PhotosetPhotoCollection>(galleryId, t => Flickr.PhotosetsGetPhotosAsync(galleryId, r => t.TrySetResult(r.Result)));
+        }
+
+        private async Task<T> TryGetFromCacheAsync<T>(string key, Action<TaskCompletionSource<T>> getFunc)
+        {
+            if (Cache.TryGetValue(key, value: out (DateTime creationDate, object entry) value) && DateTime.Now - value.creationDate < CacheTime)
+            {
+                return (T)value.entry;
+            } else
+            {
+                var t = new TaskCompletionSource<T>();
+                getFunc(t);
+                var result = await t.Task;
+                Cache[key] = (DateTime.Now, result);
+                return result;
+            }
         }
     }
 }
